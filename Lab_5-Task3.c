7271a7dd0470d8d88c327cabf2c3e422836f74ac
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2025-10-18 10:08:39 +0500

    17-October-2025

---
+++ Lab_5-Task3.c
@@ -0,0 +1,27 @@
+// Passing Multiple data in a Thread
+#include <stdio.h>
+#include <pthread.h>
+typedef struct
+{
+    int id;
+    char* message;
+}
+ThreadData;
+void* printData(void* arg)
+{
+    ThreadData* data = (ThreadData*)arg;
+    printf("Thread %d says: %s\n", data->id, data->message);
+    return NULL;
+}
+int main()
+{
+    pthread_t t1, t2;
+    ThreadData data1 = {1, "My Name is Ghulam Mohyuddin"};
+    ThreadData data2 = {2, "My CGPA: 2.78"};
+    pthread_create(&t1, NULL, printData, &data1);
+    pthread_create(&t2, NULL, printData, &data2);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    printf("All threads done.\n");
+    return 0;
+}
